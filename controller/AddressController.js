26fa70283e3deb1f7bef3e7105c8afb5c7b1e407
rizaldihabibie <rialdihabibie28>
2021-01-02 04:39:36 +0700

    fix bugs on update flow

--- controller/AddressController.js
+++ controller/AddressController.js
@@ -42,26 +42,49 @@ exports.update = function(req, res){
         if(typeof newAddress === 'undefined' || typeof newAddress === null){
             response.ok('Bad Request', 401, null, res);
         }else{
-            let encryptedData = [newAddress.id, userToken.id, newAddress.idProvince, newAddress.idCity, newAddress.idDistrict];
+            let encryptedData = [newAddress.id, userToken.id, newAddress.idDistrict];
+            let index=1;
+            if(typeof newAddress.idProvince != 'undefined' && typeof newAddress.idProvince != null){
+                encryptedData[index]=newAddress.idProvince;
+                index++;
+            }
+            if(typeof newAddress.idCity != 'undefined' && typeof newAddress.idCity != null){
+                encryptedData[index]=newAddress.idCity;
+                index++;
+            }
+            if(typeof newAddress.idDistrict != 'undefined' && typeof newAddress.idDistrict != null){
+                encryptedData[index]=newAddress.idDistrict;
+                index++;
+            }
             security.decrypt(encryptedData)
                     .then(function(decryptedId){
-                        newAddress.id = decryptedId[0];
-                        newAddress.createdBy = decryptedId[1];
-                        newAddress.idProvince = decryptedId[2];
-                        newAddress.idCity = decryptedId[3];
-                        newAddress.idDistrict = decryptedId[4];
-                        newAddress.idUser = decryptedId[0];
-                        address.update(newAddress, function(message,status,data){
-                            if(status == 200 || status == 201){
-                                if(data == null || data == ""){
-                                    response.ok('empty result', status, data, res); 
-                                }else{
-                                    response.ok(message, status, data, res);                    
-                                }
-                            }else{
-                                response.ok(message, status, null, res);            
-                            }
-                        });
+                newAddress.id = decryptedId[0];
+                newAddress.createdBy = decryptedId[1];
+                index=1;
+                if(typeof newAddress.idProvince != 'undefined' && typeof newAddress.idProvince != null){
+                    newAddress.idProvince = decryptedData[index];
+                    index++;
+                }
+                if(typeof newAddress.idCity != 'undefined' && typeof newAddress.idCity != null){
+                    newAddress.idCity == decryptedData[index];
+                    index++;
+                }
+                if(typeof newAddress.idDistrict != 'undefined' && typeof newAddress.idDistrict != null){
+                    newAddress.idDistrict = decryptedData[index];
+                    index++;
+                }
+                newAddress.idUser = decryptedId[0];
+                address.update(newAddress, function(message,status,data){
+                    if(status == 200 || status == 201){
+                        if(data == null || data == ""){
+                            response.ok('empty result', status, data, res); 
+                        }else{
+                            response.ok(message, status, data, res);                    
+                        }
+                    }else{
+                        response.ok(message, status, null, res);            
+                    }
+                });
             }).catch(function (error){
                 response.ok("data not found : "+error, 500, null, res);   
             });
