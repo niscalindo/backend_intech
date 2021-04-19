d98b6d0f752cb8c75d85c73a37911a0068ee7e8d
rizaldihabibie <rialdihabibie28>
2021-04-19 12:16:05 +0700

    working on like

--- service/ProductLike.js
+++ service/ProductLike.js
@@ -23,36 +23,65 @@ exports.find = function(security,order,findBy,id, result){
         attributes:{
             exclude: ['createdBy', 'dateCreated']
         },
-        where: whereCondition,
+        where:whereCondition
+        ,
         order: [
             ['id', order]
         ],
     }).then(data=>{
-        security.encrypt(data)
-        .then(function(encryptedData){
-            result("success", 200, encryptedData);
-        }).catch(function(error){
-            result(error, 500, null);
-        });
+        
     }).catch(err=>{
        result(err.message, 500, null);
     });
 };
 
 exports.create = function(newData,security, result){
-    newData['dateCreated'] = new Date();   
-    productLike.create(newData).then(data=>{
-        security.encrypt(data)
-        .then(function(encryptedData){
-            let newInsertedId = encryptedData.dataValues.id;
-            let newData = new Object();
-            newData['id'] = newInsertedId;
-            result("success",201,newData);
-        }).catch(function(error){
-            result(error,500,null);
-        });        
+    productLike.count({
+        attributes:{
+            exclude: ['createdBy', 'dateCreated']
+        },
+        where: {
+                id_user:{
+                    [operator.eq]:newData.idUser
+                },
+                id_product_varian:{
+                    [operator.eq]:newData.idProductVarian
+                }
+            }
+    }).then(data=>{
+        if(data > 0){
+            productLike.destroy({
+                where: {
+                        id_user:{
+                            [operator.eq]:newData.idUser
+                        },
+                        id_product_varian:{
+                            [operator.eq]:newData.idProductVarian
+                        }
+                    }
+            }).then(data=>{
+                result("success",201,data);
+            }).catch(err=>{
+                result(err.message, 500, null);
+            });
+        }else{
+            newData['dateCreated'] = new Date();   
+            productLike.create(newData).then(data=>{
+                security.encrypt(data)
+                .then(function(encryptedData){
+                    let newInsertedId = encryptedData.dataValues.id;
+                    let newData = new Object();
+                    newData['id'] = newInsertedId;
+                    result("success",201,newData);
+                }).catch(function(error){
+                    result(error,500,null);
+                });        
+            }).catch(err=>{
+                result(err.message, 500, null);
+            });             
+        }       
     }).catch(err=>{
-        result(err.message, 500, null);
+       result(err.message, 500, null);
     });
 };
 
